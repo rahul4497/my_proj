5917111b98afefc8c08e067683e0400852378289
Rahul <70948208+rahul4497@users.noreply.github.com>
2020-10-04 16:12:52 +0530

    Update wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -17,8 +17,8 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 import sys
-sys.path.append('/opt/bitnami/apps/django/django_projects/new-proj')
+sys.path.append('/opt/bitnami/apps/django/django_projects/my-proj')
 #os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/tutorial/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "new-proj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my-proj.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
