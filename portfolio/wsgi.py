8533fba7c195565bc510b1e41b23e5b8420fbce5
Rahul <70948208+rahul4497@users.noreply.github.com>
2020-10-07 15:52:44 +0530

    Update wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -6,19 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
-#import os
-
-#from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
-
-#application = get_wsgi_application()
-
 import os
 import sys
-sys.path.append('/opt/bitnami/apps/django/django_projects/my-proj')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/my-proj/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my-proj.settings")
+sys.path.append('/opt/bitnami/apps/django/django_projects/my_proj')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/my_proj/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_proj.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
